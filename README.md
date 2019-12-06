0dd5bbcd41572ba596391f8a291323c7187f4f0a
xwcdev <xwcdev@pm.me>
2019-12-06 18:14:18 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# xwc-extension-wallet
+
+### 1. Install
+
+* git submodule update --init --recursive
+* cd xwcWebWallet && npm i && npm run build
+* install this chrome extension
---
+++ inpage.js
@@ -0,0 +1,73 @@
+
+
+var webExtensionWallet = "for xwc";
+
+console.log("webExtensionWallet is defined:" + webExtensionWallet);
+
+var _XwcExtWallet = function () {
+    this.getUserAddressCallback ;
+
+    this.getUserAddress = function(callback, data) {
+        //console.log("********* get account ************")
+        getUserAddressCallback = callback
+        window.postMessage({
+            "target": "contentscript",
+            "data": data || {},
+            "method": "xwc_getUserAddress",
+        }, "*");
+    };
+
+    this.getConfigCallback ;
+
+    this.getConfig = function(callback, data) {
+        getConfigCallback = callback
+        window.postMessage({
+            "target": "contentscript",
+            "data":data || {},
+            "method": "xwc_getConfig",
+        }, "*");
+    };
+
+    this.setConfigCallback ;
+
+    this.setConfig = function(callback, data) {
+        setConfigCallback = callback
+        window.postMessage({
+            "target": "contentscript",
+            "data":data || {},
+            "method": "xwc_setConfig",
+        }, "*");
+    };
+
+    var sourceName = 'XwcExtWallet';
+
+// listen message from contentscript
+    window.addEventListener('message', function(e) {
+        // e.detail contains the transferred data (can
+        if (e.data.src ==="content" && e.data.dst === "inpage" && !!e.data.data && !!e.data.data.account && e.data.data.source===sourceName) {
+            userAddrerss = e.data.data.account;
+            var accountPubKey = e.data.data.accountPubKey;
+            var accountPubKeyString = e.data.data.accountPubKeyString
+            if(typeof getUserAddressCallback === 'function'){
+                getUserAddressCallback({address: userAddrerss, pubKey: accountPubKey, pubKeyString: accountPubKeyString})
+            }
+        }
+        if (e.data.src ==="content" && e.data.dst === "inpage" && !!e.data.data && !!e.data.data.config && e.data.data.source===sourceName) {
+            let config = e.data.data.config;
+            if(typeof getConfigCallback === 'function'){
+                getConfigCallback(config)
+            }
+        }
+        if (e.data.src ==="content" && e.data.dst === "inpage" && !!e.data.data && !!e.data.data.setConfigCallback && e.data.data.source===sourceName) {
+            let d = e.data.data.setConfigCallback;
+            if(typeof setConfigCallback === 'function'){
+                setConfigCallback(d)
+            }
+        }
+    })
+}
+
+var XwcExtWallet = new _XwcExtWallet()
+
+
+
